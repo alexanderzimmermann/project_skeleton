110edebcf056028b5add747ea08bab132dd7d12a
Alexander Zimmermann <alexander.zimmermann@pharmapp.de>
2021-01-14 13:05:41 +0100

    Adding all installed gulp packages.

--- skeleton-setup.sh
+++ skeleton-setup.sh
@@ -1,7 +1,26 @@
 #!/usr/bin/env bash
 
+# Install
+npm install --global gulp-cli
+
 # Setup all needed software for this skeleton.
 git init
 git-flow init
 composer init
 npm init
+
+# npm
+npm install --global gulp-cli
+
+# modules
+npm install --save-dev gulp
+npm install --save-dev gulp-if
+npm install --save-dev gulp-useref
+npm install --save-dev gulp-clean-css
+npm install --save-dev gulp-uglify
+
+# Composer
+composer require --dev phpunit/phpunit ^9
+
+# Additional components
+# npm install --save @progress/kendo-ui@latest
